274b8833c4fb793f65d57de31b87fe49f01a00b1
Lucas <46963678+lucasjimenezc@users.noreply.github.com>
2022-04-23 00:18:45 +0200

    Change

--- Day_10.py
+++ Day_10.py
@@ -12,11 +12,11 @@ def multiplicar(a,b):
 def dividir(a,b):
     print (f"{a} {op} {b} =  {a/b}")
     return a/b
-a = int(input("Whats the first number?: "))
+a = float(input("Whats the first number?: "))
 continuar = True
 while(continuar):
     op = input("+ \n-\n*\n/\n Pick an operation(only one of those): ")
-    b = int(input("whats the next number?: "))
+    b = float(input("whats the next number?: "))
     if op == '+':
         a = sumar(a,b)
     elif op == '-':
@@ -27,6 +27,6 @@ while(continuar):
         a = dividir(a,b)
     if(input("Quieres salir 'y' o 'n'?:") == 'n'):
         if input(f"Type 'y' to continue calculating with {a}, or type 'n' to start a new calculation: ") == 'n':
-            a = int(input("Whats the first number?: "))
+            a = float(input("Whats the first number?: "))
     else:
         continuar = False
